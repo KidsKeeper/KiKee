cd3479579eafb47d2b6f439293220b0d5ad95274
redundant4u <rafch@naver.com>
2020-06-11 16:00:06 +0900

    seol

--- flutter/safeway_direction/lib/main.dart
+++ flutter/safeway_direction/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo asdasdadHome Page'),
     );
   }
 }
