e3c30f2ca126c20d24e22e4e33344718d67a6310
seunggil1 <ksgg1navercom@gmail.com>
2020-06-19 01:50:27 +0900

    DATA BACKUP

--- flutter/safeway_direction/lib/main.dart
+++ flutter/safeway_direction/lib/main.dart
@@ -60,39 +60,73 @@ class _MyHomePageState extends State<MyHomePage> {
       extended = !extended;
       height *= extended? 1.9 : 0.5263;
       _counter++;
-      */
+      
       markerTest.add(Marker(
                markerId: MarkerId('1'),
                position: LatLng(35.2474465734972, 129.091718486654),
             ));
       markerTest.add(Marker(
                markerId: MarkerId('2'),
-               position: LatLng(35.24743597040611, 129.09151701227086)
+               position: LatLng(35.24743597040611, 129.09151701227086),
+               icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange)
             ));
       markerTest.add(Marker(
                markerId: MarkerId('3'),
                position: LatLng(35.24774149210607, 129.09153922415),
+               icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow)
             ));
       markerTest.add(Marker(
-               markerId: MarkerId('4'),
-               position: LatLng(35.24743597040611, 129.09151701227086),
-               icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
-            ));
+          markerId: MarkerId('4'),
+          position: LatLng(35.24787758810702, 129.09154755301228),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)
+      ));
       markerTest.add(Marker(
                markerId: MarkerId('5'),
-               position: LatLng(35.24787758810702, 129.09154755301228)
+               position: LatLng(35.2478794789843, 129.091691326138),  
             ));
-      LatLng origin = LatLng(35.2278421, 129.095157);
-      LatLng destination = LatLng(35.2329183, 129.082460);
-      
-      LatLng l1 = LatLng(35.2474465734972, 129.091718486654);
-      LatLng l2 = LatLng(35.2478794789843, 129.091691326138);
+      markerTest.add(Marker(
+               markerId: MarkerId('6'),
+               position: LatLng(35.2476089997793, 129.091698688253),  
+            ));
+      markerTest.add(Marker(
+               markerId: MarkerId('7'),
+               position: LatLng(35.24661944444444, 129.09141666666667), 
+               icon : BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure)
+            ));
+      markerTest.add(Marker(
+               markerId: MarkerId('8'),
+               position: LatLng(35.24841388888889, 129.091575),
+               icon : BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan)
+            ));
+
+      */
+      test();
+
+    });
+  }
+
+  void test() async{
+      LatLng l1 = LatLng(35.2451901, 129.091451);
+      LatLng l2 = LatLng(35.2487721, 129.091708);
 
       LatLng tt = LatLng(35.2476089997793, 129.091698688253);
 
-      // TmapServices.test(tt);
-      // TmapServices.getRoute(l1, l2);
-    });
+      //TmapServices.test(LatLng(35.2476089997793, 129.091698688253));
+      Map<String,dynamic> t = await TmapServices.getRoute(l1, l2);
+      for(Map<String,dynamic> iter in t['features']){
+        if(iter['geometry']['type'] == "LineString"){
+          for(var iter2 in iter['geometry']['coordinates']){
+            markerTest.add(Marker(
+               markerId: MarkerId(iter2[1].toString()),
+               position: LatLng(iter2[1], iter2[0])
+            ));
+          }
+        }
+      }
+
+      setState(() {
+        
+      });
   }
 
   @override
