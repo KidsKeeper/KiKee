e29a2adbf78c1401d5c8bc6547b74ad09dc24fab
seunggil1 <38664481+seunggil1@users.noreply.github.com>
2020-06-23 02:15:05 +0900

    change geometryFloor to static function

--- flutter/safeway_direction/lib/utility.dart
+++ flutter/safeway_direction/lib/utility.dart
@@ -15,22 +15,32 @@ class Pair<E, F> {
     if (other is! Pair) {
       return false;
     }
-    return other.first == first || other.last == last;
+    return other.first == first && other.last == last;
   }
 
   @override
   String toString() => '($first, $last)';
 
+  static Pair<double,double> geometryFloor(LatLng data, [int i = 7]){
+    String str1 = data.latitude.toString();
+    String str2 = data.longitude.toString();
+    
+    try {
+      str1 = str1.substring(0, str1.indexOf('.')+i);
+    }
+    on RangeError catch(e){
+      print(e.message);
+    }
+
+    try {
+      str2 = str2.substring(0, str2.indexOf('.')+i);
+    }
+    on RangeError catch(e){
+      print(e.message);
+    }
+
+    return Pair<double,double>(double.parse(str1), double.parse(str2));
+  }
 }
 
 
-Pair<double,double> geometryFloor(LatLng data, [int i = 7]){
-  String str1 = data.latitude.toString();
-  String str2 = data.longitude.toString();
-  
-  str1 = str1.substring(0, str1.indexOf('.')+i);
-  str2 = str2.substring(0, str2.indexOf('.')+i);
-
-  return Pair<double,double>(double.parse(str1), double.parse(str1));
-
-}
