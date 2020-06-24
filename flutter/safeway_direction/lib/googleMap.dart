84f0ad7afbb895b688cb12fd41318cd32009b0dc
seunggil1 <ksgg1navercom@gmail.com>
2020-06-24 19:34:03 +0900

    add drawPolyline

--- flutter/safeway_direction/lib/googleMap.dart
+++ flutter/safeway_direction/lib/googleMap.dart
@@ -1,12 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-const apiKey = "AIzaSyA6fqGeXFYOhuSBrVfvKoFNkN4fGAIwdmM";
+
+import 'package:safewaydirection/route.dart' as way;
+import 'package:safewaydirection/utility.dart';
+
+const _apiKey = "AIzaSyA6fqGeXFYOhuSBrVfvKoFNkN4fGAIwdmM";
 
 class GoogleMapsServices{
   // 도보 경로 탐색, 한국에서 사용 불가
   static Future<String> getRouteCoordinates(LatLng l1, LatLng l2)async{
-    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey";
+    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$_apiKey";
     http.Response response = await http.get(url);
     Map values = jsonDecode(response.body);
     print("====================>>>>>>>>$values");
@@ -16,17 +21,58 @@ class GoogleMapsServices{
 
   // 장소 정보 받아옴.
   static Future<String> searchPlace(String str) async {
-    String url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=$apiKey&inputtype=textquery&input=$str";
+    String url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=$_apiKey&inputtype=textquery&input=$str";
     url = Uri.encodeFull(url);
     http.Response response = await http.get(url);
       Map values = jsonDecode(response.body);
     print("====================>>>>>>>>$values");
     
-    url = "https://maps.googleapis.com/maps/api/place/details/json?key=$apiKey&place_id=${values['candidates'][0]['place_id']}&language=ko";
+    url = "https://maps.googleapis.com/maps/api/place/details/json?key=$_apiKey&place_id=${values['candidates'][0]['place_id']}&language=ko";
     url = Uri.encodeFull(url);
     response = await http.get(url);
     values = jsonDecode(response.body);
     print("====================>>>>>>>>$values");
     return url;
   }
+
+  static Set<Polyline> drawPolyline(way.Route routeData){
+      Set<Polyline> _polylinemarker = Set<Polyline>();
+      List<Pair<List<LatLng>,bool>> data = List<Pair<List<LatLng>,bool>>();
+      List<LatLng> n = [];
+      n.add(routeData.locations[0].location);
+      for(int i = 1; i < routeData.locations.length - 1; i++){
+        n.add(routeData.locations[i].location);
+        if(routeData.locations[i-1].danger > 0 != routeData.locations[i].danger > 0){
+          data.add(Pair(List<LatLng>.from(n),routeData.locations[i-1].danger > 0));
+          n.clear();
+          n.add(routeData.locations[i].location);
+        }
+      }
+      n.add(routeData.locations[routeData.locations.length - 1].location);
+      data.add(Pair(n,routeData.locations[routeData.locations.length-2].danger > 0));
+      
+      for(Pair<List<LatLng>,bool> iter in data){
+        if(iter.last)
+          _polylinemarker.add(
+            Polyline(
+            polylineId: PolylineId(_polylinemarker.length.toString()),
+            color: Colors.red,
+            points: iter.first
+            )
+          );
+        else
+          _polylinemarker.add(
+              Polyline(
+              polylineId: PolylineId(_polylinemarker.length.toString()),
+              color: Colors.blue,
+              points: iter.first
+              )
+            );
+      }
+      return _polylinemarker;
+  }
+
+  
+
 }
+
