fbabcb94e9da3501307129c41d405ecfdff3d3d1
seunggil1 <ksgg1navercom@gmail.com>
2020-06-28 23:41:30 +0900

    Revert "Delete main.dart"
    This reverts commit 42cee8a07e8b7f1371c808af80cc33a1abe44569.

---
+++ flutter/safeway_direction/lib/main.dart
@@ -0,0 +1,251 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:safewaydirection/api/storeInformation/store.dart' as store;
+
+import 'package:safewaydirection/route.dart' as way;
+import 'package:safewaydirection/googleMap.dart';
+import 'package:safewaydirection/tMap.dart';
+import 'package:safewaydirection/api/accidentInformation/accidentInformation.dart' as accident;
+
+import 'package:safewaydirection/detour.dart';
+
+var height = AppBar().preferredSize.height * 1.1;
+var width =  AppBar().preferredSize.width;
+
+way.Route resultRoute = way.Route();
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool extended = false;
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> markerTest = Set<Marker>();
+  Set<Polyline> _polylinemarker = Set<Polyline>();
+  Detour tour;
+  BitmapDescriptor crosswalk;
+
+  static CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(35.2464852,129.090551),
+    zoom: 14.4746,
+  );
+  void _incrementCounter() {
+      test2();
+  }
+
+
+  void test2() async{
+    print('start');
+
+    LatLng l1 = LatLng(35.2464852,129.090551);
+    LatLng l2 = LatLng(35.2487721, 129.091708);
+
+    
+    
+    _kGooglePlex = CameraPosition(
+      target: l1,
+      zoom: 14.4746,
+      );
+     tour = Detour.map(l1, l2);
+    await tour.drawAllPolyline();
+    _polylinemarker = tour.polylines;
+    
+    for(var iter in tour.routes){
+      for(var iter2 in iter.crossWalks)
+      markerTest.add(Marker(
+              markerId: MarkerId('test'+markerTest.length.toString()),
+              position: iter2,
+              icon : crosswalk
+            ));
+    }
+  setState(() {
+    
+  });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+      BitmapDescriptor.fromAssetImage(
+          ImageConfiguration(size: Size(0.2, 0.2)), 'lib/asset/crosswalk.png')
+          .then((onValue) {
+        crosswalk = onValue;
+      });
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+            preferredSize: Size.fromHeight(height),
+            child : SafeArea(
+              child: AppBar(
+
+                automaticallyImplyLeading: true,
+                flexibleSpace: Column(
+                  children: <Widget>[
+                    SafeArea( // 첫번째
+                      child : !extended ?
+                        Container(
+                          margin: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                              color: Colors.yellow[100],
+                              borderRadius : BorderRadius.all(Radius.circular(90))
+                          ),
+                          child : FlatButton(
+                              onPressed: _incrementCounter,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                      ':',
+                                      style: TextStyle(fontFamily: 'BM',fontWeight: FontWeight.bold, fontSize: 30, color: Colors.orange)
+                                  ),
+                                  Text(
+                                    'origin' + ' -> ' + 'destination',
+                                    style: TextStyle(fontFamily: 'BM',fontSize: 20, color: Colors.orange, fontWeight: FontWeight.bold),
+                                  ),
+                                  Container(
+                                      width: 40,
+                                      child : FlatButton(onPressed: _incrementCounter,
+                                        child: Container(
+                                            alignment: Alignment.center,
+                                            child : Icon(Icons.navigate_next,size : 25.0)),
+                                      )
+                                  )
+                                ],
+                              )
+                          ),
+                        )
+                        : Container(
+                        margin: EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                            color: Colors.yellow[100],
+                            borderRadius : BorderRadius.all(Radius.circular(90))
+                        ),
+                        child : FlatButton(
+                            onPressed: _incrementCounter,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                    '출발 : ',
+                                    style: TextStyle(fontFamily: 'BM',fontWeight: FontWeight.bold, fontSize: 20, color: Colors.orange)
+                                ),
+                                Text(
+                                  'origin',
+                                  style: TextStyle(fontFamily: 'BM',fontSize: 20, color: Colors.orange, fontWeight: FontWeight.bold),
+                                ),
+                                Container(
+                                    width: 40,
+                                    child : FlatButton(onPressed: _incrementCounter,
+                                      child: Container(
+                                          alignment: Alignment.center,
+                                          child : Icon(Icons.navigate_next,size : 25.0)),
+                                    )
+                                )
+                              ],
+                            )
+                        ),
+                      )
+                    ),
+                    SafeArea( // 두번쨰
+                      child : extended ?
+                        Container(
+                        alignment: Alignment.bottomCenter,
+                          child: Container(
+                            margin: EdgeInsets.all(4),
+                            alignment: Alignment.bottomCenter,
+                            decoration: BoxDecoration(
+                                color: Colors.yellow[100],
+                                borderRadius : BorderRadius.all(Radius.circular(90))
+                            ),
+                            child : FlatButton(
+                                onPressed: _incrementCounter,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                        '도착 : ',
+                                        style: TextStyle(fontFamily: 'BM',fontWeight: FontWeight.bold, fontSize: 20, color: Colors.orange)
+                                    ),
+                                    Text(
+                                      'destination',
+                                      style: TextStyle(fontFamily: 'BM',fontSize: 20, color: Colors.orange, fontWeight: FontWeight.bold),
+                                    ),
+                                    Container(
+                                        width: 40,
+                                        child : FlatButton(onPressed: _incrementCounter,
+                                          child: Container(
+                                              alignment: Alignment.center,
+                                              child : Icon(Icons.navigate_next,size : 25.0)),
+                                        )
+                                    )
+                                  ],
+                                )
+                            ),
+                          ),
+
+                      )
+                        : Container(), //null
+                    )
+                  ],
+                ),
+              )
+            ),
+        ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Opacity(opacity: extended ? 0 : 1,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                markers: markerTest,
+                polylines: _polylinemarker,
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              ),
+            ),
+            Center(
+              child: Opacity(opacity: extended ? 1 : 0,
+                child: WillPopScope(child: Text('dfd'), onWillPop: () { if(extended)_incrementCounter(); else SystemChannels.platform.invokeMethod('SystemNavigator.pop'); return;})
+              ),
+            )
+          ],
+        )
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
