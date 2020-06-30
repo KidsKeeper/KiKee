a8c1d5cdced8699a0e0620e2b4f92f5795e89e6b
redundant4u <rafch@naver.com>
2020-06-30 11:13:32 +0900

    [S] store model fix

--- docker/node/models/store.js
+++ docker/node/models/store.js
@@ -5,6 +5,7 @@ let storeSchema = new Schema({
     bizesNm: String, // 상호명
     indsSclsNm: String, // 상권업종소분류먕
     lnoAdr: String, // 지번주소
+    rdnm: String, // 도로명
     rdnmAdr: String, // 도로명주소
     lon: String, // 경도
     lat: String // 위도
