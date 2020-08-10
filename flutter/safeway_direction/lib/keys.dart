6f131ba263135fd132b38666d1b0dedb91c9a973
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2020-08-10 21:02:09 +0900

    Delete keys.dart

--- flutter/safeway_direction/lib/keys.dart
+++
@@ -1,6 +0,0 @@
-class Keys{
-  static const String googleMap =  "AIzaSyDn5hYJ8ztN5iTsSYuXfApCPQjpvQSI-Gk";
-  static const String database = "secret";
-  static const String tMap = "l7xxc6b616e8a450499fad098454b326bda9";
-  static const String place = "AIzaSyDn5hYJ8ztN5iTsSYuXfApCPQjpvQSI-Gk";
-}
