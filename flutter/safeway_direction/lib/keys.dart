a670df27b45f8c25f8678f9cd9a54bc56d2f897c
Yunjeong <ghdnsla@naver.com>
2020-08-17 00:54:04 +0900

    [YJ] descriptionCard Change

--- flutter/safeway_direction/lib/keys.dart
+++ flutter/safeway_direction/lib/keys.dart
@@ -1,6 +1,6 @@
 class Keys {
-  static const String googleMap = 'AIzaSyDgYVJNHHT88RhE-_JKwKMxCa3DubLqKvQ';
-  static const String database = 'secret';
-  static const String tMap = 'l7xxbe04abd45ea14ad29dea8cd799743db4';
-  static const String place = 'AIzaSyDgYVJNHHT88RhE-_JKwKMxCa3DubLqKvQ';
+  static const String googleMap = '******';
+  static const String database = '******';
+  static const String tMap = '****';
+  static const String place = '***';
 }
